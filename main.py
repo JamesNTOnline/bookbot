81ee031a0c2cf335207929b69d81ef4966961e0c
JamesNTOnline <james.nicholas.thomas@gmail.com>
2024-12-23 04:28:12 +0000

    sorted display of letter counts

--- main.py
+++ main.py
@@ -8,12 +8,44 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def create_char_dict(text):
+    chars = {}
+    text = text.lower()
+    for c in text:
+        if c in chars:
+            chars[c] +=1
+        else:
+            chars[c] = 1
+    return chars
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def char_dict_to_sorted_list(char_dict):
+    sorted_list = []
+    for c in char_dict:
+        if c.isalpha():
+            sorted_list.append({"char": c, "count": char_dict[c]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    print(sorted_list)
+    return sorted_list
+    
+
+
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    char_dict = create_char_dict(text)
+    sorted_char_list = char_dict_to_sorted_list(char_dict)
+    print(f"*** Report for {book_path} ***")
+    print(f"Book contains {num_words} words")
+    for c in sorted_char_list:
+        print(f"The letter {c["char"]} occurs {c["count"]} times")
+    
         
 if __name__ == "__main__":
     main()
